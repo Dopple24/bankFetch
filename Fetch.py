2cf445fd3907ee8037ec96bcf75ba2078caf9959
Dopple24 <roubradek1@gmail.com>
2025-05-01 07:12:10 +0200

    test

--- Fetch.py
+++ Fetch.py
@@ -1,5 +1,5 @@
 import requests
 
-x = requests.get('https://fioapi.fio.cz/v1/rest/last/MY177IQdqygGXpSqschQMIdcVx9DVCOdeLY59qUL9mTVJfhz1cMx8rcx7E16DoUe/transactions.json')
+x = requests.get('https://fioapi.fio.cz/v1/rest/last//transactions.json')
 
 print(x.text)
